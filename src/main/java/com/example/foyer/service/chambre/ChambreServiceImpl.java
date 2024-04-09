11bd33d6057f412b01d1de643a9bb4059309b7f7
oumaima <oumaimazanteni15@gmail.com>
2024-04-09 14:52:17 +0100

    Update ChambreServiceImpl.java

--- src/main/java/com/example/foyer/service/chambre/ChambreServiceImpl.java
+++ src/main/java/com/example/foyer/service/chambre/ChambreServiceImpl.java
@@ -7,6 +7,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional; 
+import javax.persistence.EntityNotFoundException; 
+
 
 @Service
 @AllArgsConstructor
