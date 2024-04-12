2af087b1b483946b0d806481828e8c9c3e83da30
samar rebhi <samar.rebhi@esprit.tn>
2024-04-12 14:44:06 +0100

    Update EtudiantRestController.java

--- src/main/java/com/example/foyer/controller/EtudiantRestController.java
+++ src/main/java/com/example/foyer/controller/EtudiantRestController.java
@@ -1,3 +1,4 @@
+package com.example.foyer.service.foyer.controller;
 import com.example.foyer.entity.Etudiant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.beans.factory.annotation.Autowired;
