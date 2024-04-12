575dacefe066594bf62f1457d9694531caca14dd
oumaima <oumaimazanteni15@gmail.com>
2024-04-12 17:19:35 +0100

    Update ChambreServiceImplTest.java

--- src/test/java/com/example/foyer/ChambreServiceImplTest.java
+++ src/test/java/com/example/foyer/ChambreServiceImplTest.java
@@ -3,7 +3,6 @@ package com.example.foyer;
 import com.example.foyer.entity.Chambre;
 import com.example.foyer.entity.TypeChambre;
 import com.example.foyer.service.chambre.ChambreServiceImpl;
-import com.example.foyer.service.chambre.IChambreService;
 import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
@@ -13,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -24,38 +24,35 @@ import java.util.List;
 import static com.example.foyer.entity.TypeChambre.DOUBLE;
 import static com.example.foyer.entity.TypeChambre.SIMPLE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @ExtendWith(SpringExtension.class)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@SpringBootTest
 @ActiveProfiles("test")
-
-
+@SpringBootTest
 
 public class ChambreServiceImplTest {
-    /*
     @Autowired
-ChambreServiceImpl chambreService;
-@Test
+    ChambreServiceImpl chambreService;
+    @Test
     public void testaddChambre(){
-    Chambre chambre =Chambre.builder().numeroChambre(60).typeChambre(SIMPLE).build();
-    Chambre savedChambre = chambreService.addChambre(chambre);
-    Assertions.assertNotNull(savedChambre.getIdChambre());
-    //chambreService.delete(savedChambre);
-}
+        Chambre chambre =Chambre.builder().numeroChambre(82).typeChambre(SIMPLE).build();
+        Chambre savedChambre = chambreService.addChambre(chambre);
+        Assertions.assertNotNull(savedChambre.getIdChambre());
+        //chambreService.delete(savedChambre);
+    }
 
 @Test
     public void testAddChambres() {
         List<Chambre> chambres = new ArrayList<>();
-        chambres.add(Chambre.builder().numeroChambre(55).typeChambre(TypeChambre.SIMPLE).build());
-        chambres.add(Chambre.builder().numeroChambre(56).typeChambre(TypeChambre.DOUBLE).build());
+        chambres.add(Chambre.builder().numeroChambre(603).typeChambre(TypeChambre.TRIPLE).build());
+        chambres.add(Chambre.builder().numeroChambre(608).typeChambre(TypeChambre.TRIPLE).build());
 
         List<Chambre> savedChambres = chambreService.addChambres(chambres);
         assertEquals(2, savedChambres.size());
     }
     @Test
     public void testEditChambre() {
-        Chambre chambre = Chambre.builder().idChambre(1).numeroChambre(20).typeChambre(TypeChambre.TRIPLE).build();
+        Chambre chambre = Chambre.builder().idChambre(1).numeroChambre(204).typeChambre(TypeChambre.TRIPLE).build();
         Chambre editedChambre = chambreService.editChambre(chambre);
         assertEquals(chambre.getIdChambre(), editedChambre.getIdChambre());
     }
@@ -65,6 +62,6 @@ ChambreServiceImpl chambreService;
         Assertions.assertNotNull(foundChambres);
     }
 
-*/
+
 
 }
