7a6265fe30f67234673e22fbaed40f9c62609170
samar <samar.rebhi@esprit.tn>
2024-04-12 17:17:17 +0100

    fixing tests

--- src/test/java/com/example/foyer/EtudiantServiceTest.java
+++ src/test/java/com/example/foyer/EtudiantServiceTest.java
@@ -5,18 +5,16 @@ import com.example.foyer.repository.EtudiantRepository;
 import org.junit.jupiter.api.Test;
 
 import com.example.foyer.service.etudiant.EtudiantServiceImpl;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @SpringBootTest
 public class EtudiantServiceTest {
