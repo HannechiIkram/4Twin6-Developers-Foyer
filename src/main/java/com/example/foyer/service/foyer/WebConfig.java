8112f3b60fe5aff38daf13f8366f47db562874a8
HannechiIkram <125407946+HannechiIkram@users.noreply.github.com>
2024-04-15 10:08:24 +0100

    Update WebConfig.java

--- src/main/java/com/example/foyer/service/foyer/WebConfig.java
+++ src/main/java/com/example/foyer/service/foyer/WebConfig.java
@@ -1,17 +1,1 @@
-/*package com.example.foyer.service.foyer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*") // Autorise les requêtes depuis n'importe quelle origine
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Autorise les méthodes GET, POST, PUT, DELETE
-                .allowedHeaders("Origin", "Accept", "Content-Type", "Authorization"); // Autorise uniquement les en-têtes nécessaires
-    }
-}*/
 
