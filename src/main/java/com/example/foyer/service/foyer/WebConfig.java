5bbb13414a9533cb39678e5daa2566b6303a15e7
HannechiIkram <125407946+HannechiIkram@users.noreply.github.com>
2024-04-15 10:16:05 +0100

    Update WebConfig.java

--- src/main/java/com/example/foyer/service/foyer/WebConfig.java
+++ src/main/java/com/example/foyer/service/foyer/WebConfig.java
@@ -7,10 +7,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-       
-    }
 }
 
