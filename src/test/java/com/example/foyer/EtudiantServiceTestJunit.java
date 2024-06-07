3a3052a0551f92cd9013e1f51258fa266c7cc1ab
samarrebhi <samar.rebhi@esprit.tn>
2024-06-08 00:33:04 +0100

    unit

--- src/test/java/com/example/foyer/EtudiantServiceTestJunit.java
+++ src/test/java/com/example/foyer/EtudiantServiceTestJunit.java
@@ -52,14 +52,7 @@ public class EtudiantServiceTestJunit {
 @Test
 void addEtudiantWithInvalidData() {
     // Test adding an etudiant with invalid data (e.g., null values)
-Etudiant etudiant = Etudiant.builder()
-                .nomEt()
-                .cin()
-                .email()
-                .ecole()
-                .mdp()
-                .prenomEt()
-                .build();
+
     // Create an etudiant with invalid data
     Etudiant etudiant = Etudiant.builder().build();
 
